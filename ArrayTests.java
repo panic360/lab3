35918a7cf4ad69bd89e6a847124ac0266439e602
panic360 <122554116+panic360@users.noreply.github.com>
2023-01-26 08:59:04 -0800

    Changed Tests

--- ArrayTests.java
+++ ArrayTests.java
@@ -2,17 +2,16 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class ArrayTests {
-	@Test 
-	public void testReverseInPlace() {
-    int[] input1 = { 3 };
+  @Test
+  public void testReverseInPlace() {
+    int[] input1 = new int[] {1,2,3,4,5};
     ArrayExamples.reverseInPlace(input1);
-    assertArrayEquals(new int[]{ 3 }, input1);
+    assertArrayEquals(new int[]{5,4,3,2,1}, input1);
 	}
-
 
   @Test
   public void testReversed() {
-    int[] input1 = { };
-    assertArrayEquals(new int[]{ }, ArrayExamples.reversed(input1));
+    int[] input1 = { 1,2,3,4,5 };
+    assertArrayEquals(new int[]{ 5,4,3,2,1}, ArrayExamples.reversed(input1));
   }
 }
